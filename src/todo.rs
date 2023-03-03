3f870b2f7fae1e094600793ed51036299a82b3d1
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-03-03 21:36:02 +0100

    add tests and integrated with ci

--- src/todo.rs
+++ src/todo.rs
@@ -1,12 +1,13 @@
 use serde::{Deserialize, Serialize};
+use std::cmp::Eq;
 
 use crate::random_id::random_id;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
 pub struct Todo {
     pub completed: bool,
     pub label: String,
-    pub id: String
+    pub id: String,
 }
 
 impl Todo {
@@ -16,5 +17,18 @@ impl Todo {
             id: random_id(),
             label,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Todo;
+
+    #[test]
+    fn new_is_not_completed() {
+        let todo = Todo::new(String::from("Hello world"));
+
+        assert_eq!(todo.completed, false);
+        assert_eq!(todo.label, "Hello world");
     }
 }
