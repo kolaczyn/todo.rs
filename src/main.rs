505d26aea3178e990baa40b2c203c833ec986720
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-03-06 22:05:07 +0000

    fix sql injection

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,13 @@ use tide::Request;
 
 use crate::todo::Todo;
 
-async fn get_connection() -> Result<SqliteConnection, sqlx::Error> {
-    SqliteConnection::connect("sqlite:db.sql").await
-}
-
-fn todos() -> Vec<Todo> {
-    let todo_one = Todo {
-        completed: false,
-        id: 1,
-        label: String::from("Learn Rust"),
-    };
-
-    let todo_two = Todo {
-        completed: false,
-        id: 2,
-        label: String::from("Learn Rust"),
-    };
-
-    vec![todo_one, todo_two]
-}
-
 async fn get_hello_word(_req: Request<()>) -> tide::Result<String> {
     Ok(String::from("Hello world"))
 }
 
 async fn get_todos(_req: Request<()>) -> tide::Result<String> {
     let mut conn = get_connection().await?;
-    let todos = sqlx::query_as::<_, Todo>("SELECT completed, label, id FROM todos")
+    let todos: Vec<Todo> = sqlx::query_as("SELECT completed, label, id FROM todos")
         .fetch_all(&mut conn)
         .await?;
 
@@ -41,26 +21,29 @@ async fn get_todos(_req: Request<()>) -> tide::Result<String> {
 async fn get_todo(req: Request<()>) -> tide::Result<String> {
     let id: i32 = req.param("id")?.parse()?;
     let mut conn = get_connection().await?;
-    // THIS COULD CAUSE AN SQL INJECTION! I should probably fix this :p
-    let query = format!("SELECT completed, label, id FROM todos WHERE id = {}", id).to_owned();
-    let todos = sqlx::query_as::<_, Todo>(&query)
-        .fetch_all(&mut conn)
-        .await?;
+    let todos: Vec<Todo> = sqlx::query_as(
+        r#"
+        SELECT completed, label, id
+        FROM todos
+        WHERE id = $1
+        "#,
+    )
+    .bind(id)
+    .fetch_all(&mut conn)
+    .await?;
 
     Ok(serde_json::to_string_pretty(&todos)?)
 }
 
 async fn create_todo(req: Request<()>) -> tide::Result<String> {
-    println!("Bifor");
     let label: String = req.param("label")?.parse()?;
     let mut conn = get_connection().await?;
-    // THIS WILL SURELY CAUSE AN SQL INJECTION! I should probably fix this :p
-    let query = format!(
-        "INSERT INTO todos(completed, label) VALUES({}, '{}')",
-        false, label
-    )
-    .to_owned();
-    let rows_affected = sqlx::query(&query).execute(&mut conn).await;
+
+    let rows_affected = sqlx::query("INSERT INTO todos(completed, label) VALUES($1, $2)")
+        .bind(false)
+        .bind(label)
+        .execute(&mut conn)
+        .await;
 
     match rows_affected {
         Ok(_) => Ok(String::from("Done")),
@@ -68,9 +51,14 @@ async fn create_todo(req: Request<()>) -> tide::Result<String> {
     }
 }
 
+async fn get_connection() -> Result<SqliteConnection, sqlx::Error> {
+    SqliteConnection::connect("sqlite:db.sql").await
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
+    let mut conn = get_connection();
 
     app.at("/hello").get(get_hello_word);
     app.at("/todos").get(get_todos);
--- src/todo.rs
+++ src/todo.rs
@@ -8,36 +8,9 @@ pub struct Todo {
     pub id: i32,
 }
 
-pub struct NewTodo {
-    pub label: String,
-}
-
 impl Todo {
-    pub fn new(label: String) -> Todo {
-        Todo {
-            completed: false,
-            id: Todo::random_id(),
-            label,
-        }
-    }
-    pub fn random_id() -> i32 {
-        rand::random()
-    }
     pub fn to_string(&self) -> String {
         let check = if self.completed { "[x}" } else { "[ ]" };
         format!("{} {} #{}", check, self.label, self.id)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::Todo;
-
-    #[test]
-    fn new_is_not_completed() {
-        let todo = Todo::new(String::from("Hello world"));
-
-        assert_eq!(todo.completed, false);
-        assert_eq!(todo.label, "Hello world");
     }
 }
