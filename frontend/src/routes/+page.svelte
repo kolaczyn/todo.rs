6f0ca81c4438b3864fe5b303ac063fea6ba71408
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-03-13 13:16:28 +0100

    fetch todos and categories

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to the Todo App!</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { Category, Todo } from '../types';
 
+	const endpoint: string = 'http://localhost:8080';
+
+	let todos: Todo[] = [];
+	let categories: Category[] = [];
+
+	const fetchTodos = async () => {
+		const response: Todo[] = await fetch(`${endpoint}/v1/todos`).then((x) => x.json());
+		todos = response;
+	};
+
+	const fetchCategories = async () => {
+		const response: Category[] = await fetch(`${endpoint}/v1/categories`).then((x) => x.json());
+		categories = response;
+	};
+
+	onMount(async () => {
+		fetchTodos();
+		fetchCategories();
+	});
+</script>
+
+<h1>Welcome to the Todo App!</h1>
+<h2>Todos:</h2>
+{#each todos as todo}
+	<li>{todo.id} {todo.label}</li>
+{/each}
+<h3>Categories:</h3>
+{#each categories as category}
+	<li style={`color: ${category.color};`}>{category.id} {category.label}</li>
+{/each}
---
+++ frontend/src/types.ts
@@ -0,0 +1,15 @@
+export type Todo = {
+	id: number;
+	label: string;
+	description: string | null;
+	completed: boolean;
+	category: TodoCategory | null;
+};
+
+export type TodoCategory = Category;
+
+export type Category = {
+	id: number;
+	label: string;
+	color: string;
+};
